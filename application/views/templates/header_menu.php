03e2d92d82128094500903132b1dc968d3fbe154
Andrey747 <andrewkbt197@gmail.com>
2019-10-21 10:40:46 -0700

    restaurant commits

---
+++ application/views/templates/header_menu.php
@@ -0,0 +1,31 @@
+<header class="main-header">
+    <!-- Logo -->
+    <a href="#" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>CJs</b></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>CAFE JAVA'S</b></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <div class="pull-right">
+        <div class="bs-example">
+          <div class="btn-group">
+            <button type="button" class="profile btn btn-danger dropdown-toggle" data-toggle="dropdown">Account</button>
+            <div class="dropdown-menu">
+              <a class="dropdown-ref" href="<?php echo base_url('users/profile/') ?>" class="dropdown-item"><i class="glyphicon glyphicon-user"></i><strong>Profile</strong></a>
+              <br>
+              <a class="dropdown-ref" href="<?php echo base_url('auth/logout') ?>" class="dropdown-item"><i class="glyphicon glyphicon-log-out"></i><strong>Logout</strong></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  
